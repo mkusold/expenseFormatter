52a667ac9a5c7eab0e7115e55d576fbfeeaec032
M Kusold <kusrox@gmail.com>
2024-04-01 21:34:12 -0700

    updating install script based on using it on bex
    also might require installing xcode shit but I missed copying that command

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
+# non devs probably won't have this and we need it
+touch ~/.zshrc
 # installs NVM (Node Version Manager)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 # download and install Node.js
 nvm install 20
+source ~/.zshrc
+# install the dependencies
+npm install
 echo "Now all you have to do is run npm start in this directory"
